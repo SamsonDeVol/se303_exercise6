3ac7f613e8f909f6ee2eabd9f47332ff405f6a7f
SamsonDeVol <devols@oregonstate.edu>
2022-05-08 22:00:23 -0700

    Extract class and initialize parameters into class variables

--- main.rb
+++ main.rb
@@ -5,3 +5,30 @@ def draw_button(label_text, x_position, y_position, foreground_color, is_dark_mo
     paint(label_text, x_position, y_position, foreground_color + 10, '#E0E0E0')
   end
 end
+
+class Button
+
+  attr_accessor :label_text, :x, :y, :foreground_color, :button_color
+
+  def initialize(label_text, x, y, foreground_color, button_color)
+    @label_text = label_text
+    @x_position = x_position
+    @y_position = y_position
+    @foreground_color = foreground_color
+    @button_color = button_color
+  end
+
+  def draw_button
+    paint(label_text, x_position, y_position, foreground_color, button_color)
+  end
+
+  def dark_mode
+    @button_color = '#111111'
+    @foreground_color = foreground_color - 10
+  end
+
+  def light_mode
+    @button_color = '#E0E0E0'
+    @foreground_color = foreground_color + 10
+  end
+end
